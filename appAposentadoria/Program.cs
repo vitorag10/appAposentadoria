4672e9d836f2c3b298bb6f55aa82c19b513230de
vitor <vitor.hag@outlook.com>
2023-06-06 17:48:25 -0300

    Adicionar arquivos de projeto.

---
+++ appAposentadoria/Program.cs
@@ -0,0 +1,24 @@
+﻿int anoNascimento, anoIngresso, codigoFuncionaro, idadeMinima, anosTrabalhados;
+Console.WriteLine("Informe o codigo do funcionario");
+codigoFuncionaro = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("informe o ano de nascimento");
+anoNascimento = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Informe o ano de ingresso");
+anoIngresso = Convert.ToInt32(Console.ReadLine());
+
+idadeMinima = 2022 - anoNascimento;
+anosTrabalhados = 2022 - anoIngresso;
+
+
+if (anoNascimento >= 70 && anosTrabalhados >= 30)
+{
+    Console.WriteLine("O funcionario pode se aposentar");
+}
+else if (anoNascimento >= 65 && anosTrabalhados >=30)
+{
+    Console.WriteLine("O funcionario pode se aposentar");
+}
+else
+{
+    Console.WriteLine("O funcionario não atingiu os requesitos necessarios para se aposentar");
+}
